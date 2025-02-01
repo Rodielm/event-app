fc1047738ac79357900fde29c137c79d2007ec45
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-01 07:40:44 +0100

    add event detail view

---
+++ lib/views/event_detail.view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../models/event.dart';
+
+class EventDetailView extends StatelessWidget {
+  final Event event;
+
+  const EventDetailView({super.key, required this.event});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
