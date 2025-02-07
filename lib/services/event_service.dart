50e4916a46e99cff96370f2938725ff21092d739
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-02-07 12:39:52 +0100

    add getEventById and getFilteredEvents services

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -15,6 +15,38 @@ class EventService with ChangeNotifier {
     _loadEvents();
   }
 
+  Event? getEventById(String id) {
+    return _events.firstWhere((e) => e.id == id);
+  }
+
+  List<Event> getFilteredEvents(
+      {bool showPastEvents = false,
+      bool favoritesOnly = false,
+      String sortBy = 'date'}) {
+    var filteredEvents = [..._events];
+
+    if (!showPastEvents) {
+      filteredEvents = filteredEvents
+          .where((event) => event.date.isAfter(DateTime.now()))
+          .toList();
+    }
+
+    if (favoritesOnly) {
+      filteredEvents =
+          filteredEvents.where((event) => event.isFavorite).toList();
+    }
+
+    switch (sortBy) {
+      case 'date':
+        filteredEvents.sort((a, b) => a.date.compareTo(b.date));
+        break;
+      case 'price':
+        filteredEvents.sort((a, b) => a.price.compareTo(b.price));
+        break;
+    }
+    return filteredEvents;
+  }
+
   Future<void> _loadEvents() async {
     final eventsJson = _prefs.getString(_storageKey);
     if (eventsJson != null) {
